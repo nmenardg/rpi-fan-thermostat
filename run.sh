e1021f5c5489167294728550c49727ed7b1402a4
Nicolas Ménard-Guy <nmenardg@gmail.com>
2017-08-08 22:02:21 -0400

    Fixed script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /home/pi/rpi-fan-thermostat/env/bin/activate
 
-python /home/pi/rpi-fan-thermostat/fan-thermostat > /home/pi/rpi-fan-thermostat/output.log
+python /home/pi/rpi-fan-thermostat/fan-thermostat
 
