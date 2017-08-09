a63f2f0e25c82e09c88fa98defed4748a2a2c05f
Nicolas Ménard-Guy <nmenardg@gmail.com>
2017-08-08 22:01:52 -0400

    Removed  import

--- fan-thermostat/record.py
+++ fan-thermostat/record.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 import os
 
-from variables import ROOT_DIR
-
 
 def record(target, inside_temp, outside_temp):
     now = datetime.utcnow()
-    file_name = os.path.join(ROOT_DIR, 'records', str(now.year), datetime.utcnow().strftime('%Y-%m-%d') + '.record')
+    file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'records', str(now.year), datetime.utcnow().strftime('%Y-%m-%d') + '.record')
     with open(file_name, 'a') as file:
         file.write('{timestamp}:{target};{inside};{outside}\n'.format(
             timestamp=now.isoformat(),
