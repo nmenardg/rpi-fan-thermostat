ed6a7a81035997df85a3606155a9077fb47090f1
Nicolas Ménard-Guy <nmenardg@gmail.com>
2017-08-08 22:49:50 -0400

    Create dirs

--- fan-thermostat/record.py
+++ fan-thermostat/record.py
@@ -4,7 +4,9 @@ import os
 
 def record(target, inside_temp, outside_temp):
     now = datetime.utcnow()
-    file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'records', str(now.year), datetime.utcnow().strftime('%Y-%m-%d') + '.record')
+    dir_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'records', str(now.year))
+    os.makedirs(dir_name, exist_ok=True)
+    file_name = os.path.join(dir_name, datetime.utcnow().strftime('%Y-%m-%d') + '.record')
     with open(file_name, 'a') as file:
         file.write('{timestamp}:{target};{inside};{outside}\n'.format(
             timestamp=now.isoformat(),
